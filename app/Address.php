107469c3062304f0996befd5fe9a25e1c98f74de
Adam Lopez <Adamf04@gmail.com>
2018-08-23 21:10:47 -0400

    Creating the insert or Create for users and addresses relationship

--- app/Address.php
+++ app/Address.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * need to be protected and $fillable is the namesake for the fill array
+     */
+    protected $fillable = [
+        // go to model if mass assignment exception and make name fillable
+        'name'
+
+    ];
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,18 +11,35 @@
 |
 */
 
+/* ============================================================
+ * Calling Classes to use within the routes.php
+ * ============================================================
+ * the use App\ uses the classes for User and Address
+ * There, we can set up relationships between
+ * a user and an address
+ *
+ * */
 use App\User;
 use App\Address;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
+/* ============================================================
+ * One to One Relationship CRUD
+ * ============================================================
+ * Using the insert name for the route,
+ * I am instantiating the Address object using an
+ * associative array and saving the address for
+ * user 1 using findOrFail() that finds the id or fails trying
+ * ============================================================
+ * */
 Route::get('/insert', function () {
     $user = User::findOrFail(1);
     // associative array instantiating a new object from the address
-    $address = new Address(['name'=>'1234 Fletcher Ave Tampa FL 33612']);
-    // now that the address variable is instantiated and has data, let's pull out the data
+    $address = new Address(['name'=>'9876 Dale Mabry Ave Tampa FL 33612']);
+    // now that the address variable is instantiated and has data,
+    // let's save that address for user one
     // store that data in a user variable
-    $user = address()->save($address);
+    $user->address()->save($address);
 });
